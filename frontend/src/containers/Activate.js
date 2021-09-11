9c8a34e02e52abd406086ce981e6430a68eea3d0
keki <baejaehwan1125@gmail.com>
2021-09-11 23:31:01 +0900

    first commit

---
+++ frontend/src/containers/Activate.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Activate = () => <div>activate</div>;
+
+export default Activate;
---
+++ frontend/src/containers/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../actions/auth";
+
+const Login = ({ login }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Sign in</h1>
+      <p>Sign into your account</p>
+
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength="6"
+            required
+          />
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </form>
+      <p className="mt-3">
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </p>
+      <p className="mt-3">
+        Forgot your password? <Link to="/reset-password">Reset password</Link>
+      </p>
+    </div>
+  );
+};
+
+// const mapStateToProps = state => ({
+
+// })
+
+export default connect(null, { login })(Login);
